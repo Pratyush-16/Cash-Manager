dd104e6cd6dd8ebb89e85fdf7c389673586b03a8
Pratyush <76037546+Pratyush-16@users.noreply.github.com>
2022-11-27 12:28:14 +0530

    fixed styling

--- index.js
+++ index.js
@@ -8,24 +8,29 @@ const availableNotes = [2000, 500, 100, 50,20,10,5,1];
 
 checkButton.addEventListener("click", function validatebillAndCashAmount(){
     hideMessage();
-    if (billAmount.value > 0){
-        if(cashGiven.value >= billAmount.value){
-            const amountToBeReturned = cashGiven.value - billAmount.value;
+    if (Number(billAmount.value > 0)){
+        if(Number(cashGiven.value) >= Number(billAmount.value)){
+            const amountToBeReturned = (Number(cashGiven.value)  - Number(billAmount.value));
             calculateChange(amountToBeReturned);
         }else{
             showMessage("Do you want to wash dishes !!");
         }
-    }
+    } h
     else{
         showMessage("Invalid Bill Amount");
     }
 });
 
 function calculateChange(amountToBeReturned){
+    // for(let i=0; i< availableNotes.length; i++){
+    //     var numberOfNotes = Math.trunc(amountToBeReturned/ availableNotes[i]);
+    //     amountToBeReturned %= availableNotes[i];
+    //     noOfNotes[i].innerText = noOfNotes;
+    // }
     for(let i=0; i< availableNotes.length; i++){
         const numberOfNotes = Math.trunc(amountToBeReturned/ availableNotes[i]);
         amountToBeReturned %= availableNotes[i];
-        noOfNotes[i].innerText = noOfNotes;
+        numberOfNotes[i].innerText = numberOfNotes 
     }
 }
 
