1452db80ea84eed61845b85e04d8268c9a98ce04
Pratyush <76037546+Pratyush-16@users.noreply.github.com>
2022-11-27 12:42:48 +0530

    fixed table

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 const billAmount = document.querySelector("#bill-amount");
 const cashGiven = document.querySelector("#cash-given");
 const checkButton =document.querySelector("#check-button");
+const output = document.querySelector("output");
 const message = document.querySelector("#error-message");
 const noOfNotes = document.querySelectorAll(".no-of-notes");
 
 const availableNotes = [2000, 500, 100, 50,20,10,5,1];
 
-checkButton.addEventListener("click", function validatebillAndCashAmount(){
+checkButton.addEventListener("click", function checkError(){
     hideMessage();
+    if(cashGiven.value==''||billAmount.value == ''){
+        output.innerText = 'Please insert both the values';
+    }else{
+        validateFunction();
+    }
+    
+});
+
+function validatebillAndCashAmount(){
     if (Number(billAmount.value > 0)){
         if(Number(cashGiven.value) >= Number(billAmount.value)){
             const amountToBeReturned = (Number(cashGiven.value)  - Number(billAmount.value));
@@ -15,11 +25,11 @@ checkButton.addEventListener("click", function validatebillAndCashAmount(){
         }else{
             showMessage("Do you want to wash dishes !!");
         }
-    } h
+    } 
     else{
         showMessage("Invalid Bill Amount");
     }
-});
+}
 
 function calculateChange(amountToBeReturned){
     // for(let i=0; i< availableNotes.length; i++){
