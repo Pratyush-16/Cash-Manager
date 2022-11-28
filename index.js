1240c4a6a65d15733001b4e421c11bad3ef52834
Pratyush <76037546+Pratyush-16@users.noreply.github.com>
2022-11-28 23:04:02 +0530

    minor fixes in function

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const cashGiven = document.querySelector("#cash-given");
 const checkButton =document.querySelector("#check-button");
 const showOutput = document.querySelector("#output");
 const message = document.querySelector("#error-message");
-const noOfNotes = document.querySelectorAll(".no-of-notes");
+const noOfNotes = document.querySelectorAll(".notes");
 
-const availableNotes = [2000, 500, 100, 50,20,10,5,1];
+const availableNotes = [2000, 500, 100, 50,10,5,1];
 
 checkButton.addEventListener("click", function checkError(){
     
@@ -20,14 +20,11 @@ checkButton.addEventListener("click", function checkError(){
 function validateBillAndCashAmount(){
     hideMessage();
     //checking the bill is greater than zero
-
     const billamount = Number(billAmount.value);
     const cashgiven = Number(cashGiven.value);
-
     if (cashgiven >0 && billamount >0) {
         //chck bill is less than cash or not, if less then pay the bill
-
-        if( cashgiven >= billamount){
+    if( cashgiven >= billamount){
 
             const amountToBeReturn = cashgiven - billamount;
             calculateChange(amountToBeReturn);
@@ -38,26 +35,19 @@ function validateBillAndCashAmount(){
         }
     } 
     else{
-
         //if bill is zero or negative
         showMessage("Invalid Bill Amount");
     }
 }
 
 function calculateChange(amountToBeReturn){
-    // for(let i=0; i< availableNotes.length; i++){
-    //     var numberOfNotes = Math.trunc(amountToBeReturned/ availableNotes[i]);
-    //     amountToBeReturned %= availableNotes[i];
-    //     noOfNotes[i].innerText = noOfNotes;
-    // }
     showOutput.innerText = 'Amount to be return is: ' + amountToBeReturn;
 
     for(let i=0; i< availableNotes.length; i++){
 
-        const numberOfNotes = Math.trunc(amountToBeReturn/ availableNotes[i]);
-        amountToBeReturn = amountToBeReturn % availableNotes[i];
-        //numberOfNotes[i].innerText = numberOfNotes; 
-        numberOfNotes[i].innerText = noOfNotes;
+        var numberOfNotes = Math.trunc(amountToBeReturn/ availableNotes[i]);
+        amountToBeReturn = amountToBeReturn % availableNotes[i]; 
+        noOfNotes[i].innerText = numberOfNotes;
     }
 }
 
