80144d3721a7e429744ad336736958d9d0922bb3
Pratyush <76037546+Pratyush-16@users.noreply.github.com>
2022-11-28 16:49:31 +0530

    changes in functions

--- index.js
+++ index.js
@@ -1,46 +1,63 @@
 const billAmount = document.querySelector("#bill-amount");
 const cashGiven = document.querySelector("#cash-given");
 const checkButton =document.querySelector("#check-button");
-const output = document.querySelector("output");
+const showOutput = document.querySelector("#output");
 const message = document.querySelector("#error-message");
 const noOfNotes = document.querySelectorAll(".no-of-notes");
 
 const availableNotes = [2000, 500, 100, 50,20,10,5,1];
 
 checkButton.addEventListener("click", function checkError(){
-    hideMessage();
+    
     if(cashGiven.value==''||billAmount.value == ''){
-        output.innerText = 'Please insert both the values';
+    showOutput.innerText = 'Enter both values';
     }else{
-        validateFunction();
+        validateBillAndCashAmount();
     }
     
 });
 
-function validatebillAndCashAmount(){
-    if (Number(billAmount.value > 0)){
-        if(Number(cashGiven.value) >= Number(billAmount.value)){
-            const amountToBeReturned = (Number(cashGiven.value)  - Number(billAmount.value));
-            calculateChange(amountToBeReturned);
+function validateBillAndCashAmount(){
+    hideMessage();
+    //checking the bill is greater than zero
+
+    const billamount = Number(billAmount.value);
+    const cashgiven = Number(cashGiven.value);
+
+    if (cashgiven >0 && billamount >0) {
+        //chck bill is less than cash or not, if less then pay the bill
+
+        if( cashgiven >= billamount){
+
+            const amountToBeReturn = cashgiven - billamount;
+            calculateChange(amountToBeReturn);
         }else{
+
+            //if bill is more than cash
             showMessage("Do you want to wash dishes !!");
         }
     } 
     else{
+
+        //if bill is zero or negative
         showMessage("Invalid Bill Amount");
     }
 }
 
-function calculateChange(amountToBeReturned){
+function calculateChange(amountToBeReturn){
     // for(let i=0; i< availableNotes.length; i++){
     //     var numberOfNotes = Math.trunc(amountToBeReturned/ availableNotes[i]);
     //     amountToBeReturned %= availableNotes[i];
     //     noOfNotes[i].innerText = noOfNotes;
     // }
+    showOutput.innerText = 'Amount to be return is: ' + amountToBeReturn;
+
     for(let i=0; i< availableNotes.length; i++){
-        const numberOfNotes = Math.trunc(amountToBeReturned/ availableNotes[i]);
-        amountToBeReturned %= availableNotes[i];
-        numberOfNotes[i].innerText = numberOfNotes 
+
+        const numberOfNotes = Math.trunc(amountToBeReturn/ availableNotes[i]);
+        amountToBeReturn = amountToBeReturn % availableNotes[i];
+        //numberOfNotes[i].innerText = numberOfNotes; 
+        numberOfNotes[i].innerText = noOfNotes;
     }
 }
 
